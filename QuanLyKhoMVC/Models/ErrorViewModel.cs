67fde6a6d990e20c2b9d69d3f9319432bab67dfc
dipthong7119 <dtc245010048@ictu.edu.vn>
2026-04-16 15:48:58 +0700

    Delete QuanLyKhoMVC directory nen xoa

--- QuanLyKhoMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QuanLyKhoMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
